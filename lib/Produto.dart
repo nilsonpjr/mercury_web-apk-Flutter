4bbfabd3e923e0028dfb64571820357501852451
nilsonpjr <nilsonpjr@gmail.com>
2022-06-18 22:45:58 -0300

    varias alteraçoes

--- lib/Produto.dart
+++ lib/Produto.dart
@@ -1,18 +1,24 @@
 // ignore_for_file: file_names
 
-class Produto{
+class Produto {
   String codigo;
   String descricao;
   String valorCusto;
+  String valorTabela;
+  String valorVenda;
+  String qtdaEst;
 
-  Produto(this.codigo,this.descricao,this.valorCusto);
+  Produto(this.codigo, this.descricao, this.valorCusto, this.valorTabela,
+      this.valorVenda, this.qtdaEst);
   Produto.fromJson(Map json)
       : codigo = json['codigo'],
-      descricao = json['descricao'],
-      valorCusto = json['valorCusto'];
-         //print(Produto.fromJson(json));
-  Map toJson()
-  {
-    return {'codigo':codigo, 'descricao':descricao, 'valorCusto':valorCusto};
+        descricao = json['descricao'],
+        valorCusto = json['valorCusto'],
+        valorTabela = json['valorTabela'],
+        valorVenda = json['valorVenda'],
+        qtdaEst = json['qtdaEst'];
+  //print(Produto.fromJson(json));
+  Map toJson() {
+    return {'codigo': codigo, 'descricao': descricao, 'valorCusto': valorCusto, 'valorTabela': valorTabela, 'valorVenda': valorVenda, 'qtdaEst': qtdaEst};
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -26,34 +26,41 @@ class BuildListView extends StatefulWidget {
   const BuildListView({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _BuildListViewState createState() => _BuildListViewState();
 }
 
 class _BuildListViewState extends State<BuildListView> {
   var produtos = <Produto>[];
   String itempesquisa = '33395';
+  var _controller = TextEditingController();
+
   final RoundedLoadingButtonController _btnController2 =
+      RoundedLoadingButtonController();
+  final RoundedLoadingButtonController _btnController3 =
       RoundedLoadingButtonController();
   String itemerror = '';
 
   _getUsers(String itempresquisa) {
     API.getPreco(itempresquisa).then((response) {
       setState(() {
-         if (response.statusCode != 200) {
-           _btnController2.error();
-           itemerror = 'Erro Servidor';
-         }
-      {
-        Iterable lista = json.decode(response.body);
-        produtos = lista.map((model) => Produto.fromJson(model)).toList();
-        _btnController2.stop();
-      };
+
+        if (response.statusCode != 200) 
+        {
+          _btnController2.error();
+          _btnController3.reset();
+          itemerror = 'Erro Servidor';
+        }
+        {
+          Iterable lista = json.decode(response.body);
+          produtos = lista.map((model) => Produto.fromJson(model)).toList();
+          itemerror = '';
+          _btnController2.stop();
+          _btnController3.stop();
+        }
       });
     });
   }
-
-  // Never newMethod() => throw Exception("API error");
-  // _BuildListViewState(){_btnController2.stop();}
 
   @override
   Widget build(BuildContext context) {
@@ -66,62 +73,95 @@ class _BuildListViewState extends State<BuildListView> {
           children: <Widget>[
             const SizedBox(height: 20),
             Flexible(
+                flex: 1,
                 child: TextField(
                     onChanged: (String text) {
                       itempesquisa = text;
                       FontWeight.bold;
                     },
+                    controller: _controller,
                     decoration: const InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: 'Digite o codido do descrição do item',
                         hintText: 'Digite aqui!',
-                        
-                        isDense: true)),
-                flex: 1),
+                        isDense: true))),
             const SizedBox(height: 20),
             Flexible(
+                flex: 1,
                 child: RoundedLoadingButton(
                   color: Colors.blue,
                   successColor: Colors.blue,
                   onPressed: () {
                     _getUsers(itempesquisa);
+                    _btnController3.reset();
                   },
                   valueColor: Colors.black,
                   borderRadius: 10,
                   controller: _btnController2,
                   child: const Text('Pesquisar',
                       style: TextStyle(color: Colors.white)),
-                ),
-                flex: 1),
-            SizedBox(height: 20),
+                )),
+            const SizedBox(height: 20),
             Flexible(
+                flex: 1,
+                child: RoundedLoadingButton(
+                  color: Colors.blue,
+                  successColor: Colors.blue,
+                  onPressed: () {
+                    _btnController2.reset();
+                    _btnController3.reset();
+                    _controller.clear();
+                  },
+                  valueColor: Colors.black,
+                  borderRadius: 10,
+                  controller: _btnController3,
+                  child: const Text('Voltar',
+                      style: TextStyle(color: Colors.white)),
+                )),
+            const SizedBox(height: 20),
+            Flexible(
+                flex: 8,
                 child: ListView.builder(
                     itemCount: produtos.length,
                     itemBuilder: (BuildContext context, int index) {
                       return GestureDetector(
                           onTap: () {
                             _btnController2.start();
+                            _btnController3.reset();
                             _getUsers("rotor");
                           },
                           child: ListTile(
-                            title: Text(
-                              '----------------' + itemerror + '------------------------------\nCodigo =' +
-                                  produtos[index].codigo +
-                                  '\nDescricao = ' +
-                                  produtos[index].descricao,
-                              style: const TextStyle(
-                                  fontSize: 20.0, color: Colors.black),
+                            title: Visibility(
+                              visible: (itemerror != 'Erro Servidor'),
+                              replacement: Text('-$itemerror-',
+                                style: const TextStyle(
+                                    fontSize: 20.0,
+                                    color: Color.fromARGB(255, 0, 0, 0)),
+                              ),
+                              child: Text(
+                                '-$itemerror-\nCodigo =${produtos[index].codigo}\nDescricao = ${produtos[index].descricao}\nValor tabela = ${produtos[index].valorTabela}\nValor Venda = ${produtos[index].valorVenda}\nValor Custo = ${produtos[index].valorCusto}',
+                                style: const TextStyle(
+                                    fontSize: 20.0,
+                                    color: Color.fromARGB(255, 0, 0, 0)),
+                              ),
                             ),
-                            subtitle: Text(
-                              'Valor Custo = ' +
-                                  produtos[index].valorCusto +
-                                  '\n',
-                              style: const TextStyle(
-                                  fontSize: 20.0, color: Colors.black),
+                            subtitle: Visibility(
+                              visible: (produtos[index].qtdaEst != '0'),
+                              replacement: Text(
+                                '\nQtda Estoque = ${produtos[index].qtdaEst}\n',
+                                style: const TextStyle(
+                                    fontSize: 20.0,
+                                    color: Color.fromARGB(255, 220, 37, 5)),
+                              ),
+                              child: Text(
+                                '\nQtda Estoque = ${produtos[index].qtdaEst}\n',
+                                style: const TextStyle(
+                                    fontSize: 20.0,
+                                    color: Color.fromARGB(255, 71, 198, 29)),
+                              ),
                             ),
                           ));
-                    }),
-                flex: 8),
+                    })),
           ],
         ));
   }
